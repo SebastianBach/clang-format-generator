18b6d9cf8994b75d68c037646093e8033af3388b
Sebastian Bach <bachsebastian@gmx.de>
2024-02-10 01:01:17 +0100

    code coverage report

---
+++ build_coverage.sh
@@ -0,0 +1,8 @@
+mkdir build
+cd build
+mkdir coverage
+cmake -DBUILD_COVERAGE=ON ..
+cmake --build . --target clean
+cmake --build . 
+ctest  -VV
+gcovr -r .. --html --html-details -o coverage/coverage.html
