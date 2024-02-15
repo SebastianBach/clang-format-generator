e1b88286416547a152783b59debe83f27dbd2095
Sebastian Bach <bachsebastian@gmx.de>
2024-02-15 20:24:40 +0100

    examples

---
+++ examples/style_a.cpp
@@ -0,0 +1,28 @@
+
+///////////////////////////////////// MAX WIDTH /////////////////////////////////////////////
+
+namespace lib{
+
+enum TYPES{
+    TYPE_A,
+    TYPE_B,
+};
+
+struct test_data{
+    float data;
+};
+
+class ReferenceClass{
+public:
+    ReferenceClass(int* value, float& ref){
+        if(value){
+            int a = 5;
+        }else{
+            int a = 6;
+        }
+    }
+private:
+    int values[5];
+};
+
+}
